bcb24cae481e8bc87ae7faeb84f86a269e6ace03
Sherline Kinyanjui <sherlinekinyanjui@gmail.com>
2020-09-22 21:33:59 +0300

    HomeScreen

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import HomeScreen from './Screens/HomeScreen';
+
+const initialState = {
+  action: '',
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'OPEN_MENU':
+      return {action: 'openMenu'};
+    case 'CLOSE_MENU':
+      return {action: 'closeMenu'};
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+const App = () => (
+  <Provider store={store}>
+    <HomeScreen />
+  </Provider>
+);
+
+export default App;
