a52edbb845996b2ec6f9e9b78787ff5bf3c5ef24
Sherline Kinyanjui <sherlinekinyanjui@gmail.com>
2020-09-25 19:01:05 +0300

    path update

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {createStore} from "redux";
 import {Provider} from "react-redux";
-import HomeScreen from "./Screens/HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
 import AppNavigator from "./navigator/AppNavigator";
 
 const initialState = {
